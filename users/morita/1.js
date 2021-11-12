592c61b088a1612d791b9599f3ec182a5c557c80
kenmori <kenjimoritata@gmail.com>
2021-11-13 00:22:49 +0900

    2 files modified

--- users/morita/1.js
+++ users/morita/1.js
@@ -1,1 +1,12 @@
-console.log('here!!');
+// example
+
+// input ["a"]
+// output ["b"]
+
+// your implements here
+function input(a) {
+  return (a[0] = 'b');
+}
+
+const output = input(['a']);
+console.log(output);
