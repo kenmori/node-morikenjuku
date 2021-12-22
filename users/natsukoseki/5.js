c8a4fdb6626faa122df7b93c804f7a9693ceb71f
NatsukoSeki <seki72ko@gmail.com>
2021-12-22 16:32:34 +0900

    add:Add a different version,For Object.assign method.

--- users/natsukoseki/5.js
+++ users/natsukoseki/5.js
@@ -1,10 +1,22 @@
 // input:["a", "b", "c"]
 // output:{0: "a", 1, "b", 2: "c"}
 
-function f(input) {
-	return { ...input };
+{
+	function f(input) {
+		return { ...input };
+	}
+
+	const array = ['a', 'b', 'c'];
+	const output = f(array);
+	console.log(output);
 }
 
-const array = ['a', 'b', 'c'];
-const output = f(array);
-console.log(output);
+{
+	function f(input) {
+		return Object.assign({}, input);
+	}
+
+	const array = ['a', 'b', 'c'];
+	const output = f(array);
+	console.log(output);
+}
