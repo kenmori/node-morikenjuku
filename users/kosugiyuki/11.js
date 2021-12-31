b9ded5c942798353b0d066d3e7e80d2a28f5cfd6
hippopo-moo <hippopo.web@gmail.com>
2021-12-31 15:48:26 +0900

    feat: work11

---
+++ users/kosugiyuki/11.js
@@ -0,0 +1,29 @@
+// input
+// [1, 2, 3, 4]
+// output
+// {
+//   sum: 10,
+//   ids: [
+//     {
+//       1: { value: 1 },
+//       2: { value: 2 },
+//       3: { value: 3 },
+//       4: { value: 4 }
+//     }
+//   ]
+// }
+const input = [1, 2, 3, 4];
+
+const f = (param) => {
+  return param.reduce(
+    (obj, current, index) => {
+      obj.sum = obj.sum + current;
+      obj.ids[index] = { ['value']: current };
+      return obj;
+    },
+    { sum: 0, ids: {} }
+  );
+};
+
+const output = f(input);
+console.log(output);
