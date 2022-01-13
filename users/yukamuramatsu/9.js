ce0004ae426bad36bb34a19ce33883df70c7e767
yuka830 <yuka.m08@gmail.com>
2022-01-12 23:07:45 -0800

    1 file added

---
+++ users/yukamuramatsu/9.js
@@ -0,0 +1,13 @@
+function f(input) {
+  const result = input.includes('e');
+  return result;
+}
+const input = ['a', 'b', 'c'];
+const output = f(input);
+console.log(output);
+
+// input
+// ["a", "b", "c"]
+
+// output
+// false
