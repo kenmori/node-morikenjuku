b34b93e981b302f736cf6258018813ecd5ff7618
csakyo <c.sakyou@gmail.com>
2022-02-12 14:41:10 +0900

    feat: task04

---
+++ users/chihirosakyo/4.js
@@ -0,0 +1,8 @@
+function f(input){
+  const result =input.filter( value => ((value % 2) != 0) )
+  return result;
+}
+
+const input = [1, 2, 3, 4, 5, 6, 7];
+const output = f(input);
+console.log(output);
