004cd6d05d2f9736883f84dd73bb8da1c6fb16e0
haru-programming <h136479442@gmail.com>
2022-03-20 20:32:02 +0900

    refactor: fix function name to lowercase the first letter

--- users/takedaharuna/5.js
+++ users/takedaharuna/5.js
@@ -1,10 +1,10 @@
 //input: ["a", "b", "c"]
 //output: {0: "a", 1: "b", 2: "c"}
 
-function ConvertToObject(input) {
+function convertToObject(input) {
     return {...input};
 }
 
 const input = ["a", "b", "c"];
-const output = ConvertToObject(input);
+const output = convertToObject(input);
 console.log(output);
